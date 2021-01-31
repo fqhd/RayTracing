de4a68eed3edb5c6ca838891cdc18c3a0876f8bc
fqhd <iprograminjava@gmail.com>
2021-01-31 09:52:18 +0100

    removed license

--- main.cpp
+++ main.cpp
@@ -4,13 +4,17 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
 	vec3 oc = r.origin() - center;
 	auto a = dot(r.direction(), r.direction());
 	auto b = 2.0 * dot(oc, r.direction());
 	auto c = dot(oc, oc) - radius*radius;
 	auto discriminant = b*b - 4*a*c;
-	return (discriminant > 0);
+	if (discriminant < 0) {
+		return -1.0;
+	} else {
+		return (-b - sqrt(discriminant) ) / (2.0*a);
+	}
 }
 
 color ray_color(const ray& r) {
